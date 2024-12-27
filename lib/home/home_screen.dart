70f3678c55d1aa7414ecf8891417d4c7f78414ae
HeshamSeliem <heshamseliem9@gmail.com>
2024-12-26 22:26:57 -0800

    add search logic

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:islamy_app/home/buttom_nav.dart';
 import 'package:islamy_app/home/tabs/ahadeth_tab.dart';
 import 'package:islamy_app/home/tabs/azkar_tab.dart';
-import 'package:islamy_app/home/tabs/quran_tab.dart';
+import 'package:islamy_app/home/tabs/quran_tab/quran_tab.dart';
 import 'package:islamy_app/home/tabs/radio_tab.dart';
 import 'package:islamy_app/home/tabs/sebha_tab.dart';
 
@@ -14,64 +15,35 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-   int currentIndex = 0;
-
+ int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 200,
-      height: 200,
       decoration:  BoxDecoration(
         image: DecorationImage(image: AssetImage("assets/images/${getBackGroundImage()}",
         ),
         fit: BoxFit.cover
         )
       ),
-       child:  Scaffold(
+       child:  Scaffold( 
         backgroundColor: Colors.transparent,
-        bottomNavigationBar: BottomNavigationBar(
-          showSelectedLabels: true,
-          showUnselectedLabels: false,
-          backgroundColor:const Color(0xffE2BE7F),
-          selectedItemColor:  Colors.white,
-         currentIndex: currentIndex,
-         onTap: (value) {
-           currentIndex = value;
-           setState(() {
-             
-           });
-         },
-         type: BottomNavigationBarType.fixed,
-       
-          items: [
-        BottomNavigationBarItem(
-          icon: _buildNavIcon("quran", 0),
-          label: "Quran" ),
-           BottomNavigationBarItem(
-          icon: _buildNavIcon("ahadeth", 1),
-          label: "Ahadeth" ),
-           BottomNavigationBarItem(
-          icon: _buildNavIcon("sebha", 2),
-          label: "Sabeh" ),
-           BottomNavigationBarItem(
-          icon: _buildNavIcon("radio", 3),
-          label: "Radio" ),
-            BottomNavigationBarItem(
-          icon: _buildNavIcon("azkar", 4),
-          label: "Azkar" ),
-
-        ],),
+        bottomNavigationBar: ButtomNavBar(onChanged: (index){
+        currentIndex = index;
+        setState(() {
+          
+        });
+        },),
         body: tabs[currentIndex],
-
-      ),
+       
+             ),
     );
   }
   List<Widget> tabs =[
-    QuranTab(),
-    AhadethTab(),
-    SebhaTab(),
-    RadioTab(),
-    AzkarTab()
+     QuranTab(),
+    const AhadethTab(),
+    const SebhaTab(),
+    const RadioTab(),
+    const AzkarTab()
   ];
   String getBackGroundImage()
   {
@@ -85,16 +57,5 @@ class _HomeScreenState extends State<HomeScreen> {
     }
 
   }
-  Widget _buildNavIcon(String imageName, int index)
-  {
-    return (currentIndex == index)? Container(
-          padding: const EdgeInsets.symmetric(vertical: 6 ,horizontal: 20),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(60),
-            color: const Color(0x99202020),
-          ),
-          child: ImageIcon(AssetImage("assets/images/$imageName.png"))
-          ):ImageIcon(AssetImage("assets/images/$imageName.png"))
-          ;
-  }
+ 
 }
